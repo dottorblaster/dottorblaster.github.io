b9679373721a56a8cb1566c3b9f45cb2919eb956
Alessio Biancalana <alessio@dottorblaster.it>
2024-04-17 18:14:07 +0200

    Remove vim provisioning from file tree

--- vim_provision.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-git clone https://github.com/reedes/vim-pencil ~/.vim/bundle/vim-pencil
-git clone git@github.com:junegunn/goyo.vim.git ~/.vim/bundle/goyo.vim
